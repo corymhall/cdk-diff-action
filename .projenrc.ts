e582c7f901814b4ac3e6676513e381232627670d
corymhall <43035978+corymhall@users.noreply.github.com>
2023-10-21 06:03:43 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,12 @@
+import { GitHubActionTypeScriptProject } from 'projen-github-action-typescript';
+const project = new GitHubActionTypeScriptProject({
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-github-action-typescript'],
+  name: 'cdk-diff-action',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
